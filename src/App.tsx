68f28c55d568ea6aa6f490b9c57d51b608059d95
RdotSilva <RyandotSilva@gmail.com>
2023-04-09 08:14:22 -0400

    Add NavBar to main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import * as React from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Grid,
-  theme,
-} from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
+import { NavBar } from "./components/Navbar";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <NavBar />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
