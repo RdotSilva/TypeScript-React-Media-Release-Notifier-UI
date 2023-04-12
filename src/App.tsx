76477985363e4c523f56379e0c1fd5db55594ed3
RdotSilva <RyandotSilva@gmail.com>
2023-04-12 19:37:27 +0100

    Fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { NavBar } from "./components/Navbar";
+import { NavBar } from "./components/NavBar/NavBar";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <NavBar />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-       
+        Welcome to Release Notifier
       </Grid>
     </Box>
   </ChakraProvider>
