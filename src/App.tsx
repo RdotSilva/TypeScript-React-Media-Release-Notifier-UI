b061030a44e7a916ee41fd10ff2466941877b1be
RdotSilva <RyandotSilva@gmail.com>
2023-04-13 20:57:13 +0100

    Add router to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar/NavBar";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./components/Router/Router";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <NavBar />
+    <RouterProvider router={router} />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         Welcome to Release Notifier
