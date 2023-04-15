f47195b49c9828095777deaf2571cdb2e07fee09
RdotSilva <RyandotSilva@gmail.com>
2023-04-15 18:34:08 +0100

    Fix main App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
-import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
-import { NavBar } from "./components/NavBar/NavBar";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/Router/Router";
+import Layout from "./components/Layout/Layout";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <NavBar />
-    <RouterProvider router={router} />
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        Welcome to Release Notifier
-      </Grid>
-    </Box>
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
   </ChakraProvider>
 );
