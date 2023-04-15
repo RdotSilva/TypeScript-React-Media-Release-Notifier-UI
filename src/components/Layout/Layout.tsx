0f680fbfe4172fd1df27a5c55aa95ea6120ebd79
RdotSilva <RyandotSilva@gmail.com>
2023-04-15 18:33:19 +0100

    Add prop types

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { NavBar } from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <NavBar />
