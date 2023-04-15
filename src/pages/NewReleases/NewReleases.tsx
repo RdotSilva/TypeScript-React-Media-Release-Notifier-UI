aa43c9bac9174b10fc62ac866fab6cf9008e7334
RdotSilva <RyandotSilva@gmail.com>
2023-04-15 18:40:32 +0100

    Add component for NewReleases

---
+++ src/pages/NewReleases/NewReleases.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "@chakra-ui/react";
+import React from "react";
+
+const NewReleases = () => {
+  return (
+    <Center bg="#2D3748" h="100px" color="white">
+      <Text fontSize="6xl"> New Releases</Text>
+    </Center>
+  );
+};
+
+export default NewReleases;
