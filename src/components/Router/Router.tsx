8b8592ccc7c2abb2d57170bb970525a58d11471c
RdotSilva <RyandotSilva@gmail.com>
2023-04-15 18:40:39 +0100

    Update router with new page

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
+import NewReleases from "../../pages/NewReleases/NewReleases";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/newreleases",
+    element: <NewReleases />,
   },
 ]);
