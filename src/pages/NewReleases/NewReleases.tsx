49ab526fab536b70be74bf68bfec5630fbfde73f
RdotSilva <RyandotSilva@gmail.com>
2023-04-16 20:40:01 +0100

    Update NewReleases to use movie card

--- src/pages/NewReleases/NewReleases.tsx
+++ src/pages/NewReleases/NewReleases.tsx
@@ -1,10 +1,14 @@
 import { Center, Text } from "@chakra-ui/react";
 import React from "react";
+import MovieCard from "../../components/MovieCard/MovieCard";
 
-const NewReleases = () => {
+const NewReleases = (newReleases) => {
   return (
     <Center bg="#2D3748" h="100px" color="white">
       <Text fontSize="6xl"> New Releases</Text>
+      {newReleases.map((release) => (
+        <MovieCard {...release} />
+      ))}
     </Center>
   );
 };
