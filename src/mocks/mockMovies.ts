f152b970193906d51000af979211b9a9dc514942
RdotSilva <RyandotSilva@gmail.com>
2023-04-17 21:54:09 +0100

    Add mock movies

---
+++ src/mocks/mockMovies.ts
@@ -0,0 +1,10 @@
+export const mockMovies = [
+  {
+    img: "image",
+    title: "1",
+    description: "Movie 1",
+    releaseDate: "4/17/23",
+    linkOne: "http://www.linkOne.com",
+    linkTwo: "http://www.linktwo.com",
+  },
+];
