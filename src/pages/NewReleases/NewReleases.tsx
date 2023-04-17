9bda038413602a34afcf4b51599cbe920567f91b
RdotSilva <RyandotSilva@gmail.com>
2023-04-17 22:02:00 +0100

    Update display for new movies

--- src/pages/NewReleases/NewReleases.tsx
+++ src/pages/NewReleases/NewReleases.tsx
@@ -1,15 +1,16 @@
 import { Center, Text } from "@chakra-ui/react";
 import React from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
+import { mockMovies } from "../../mocks/mockMovies";
 
-const NewReleases = (newReleases) => {
+const NewReleases = () => {
   return (
-    <Center bg="#2D3748" h="100px" color="white">
+    <>
       <Text fontSize="6xl"> New Releases</Text>
-      {newReleases.map((release) => (
+      {mockMovies.map((release) => (
         <MovieCard {...release} />
       ))}
-    </Center>
+    </>
   );
 };
 
