6dc88cd539ffa952a62ea6de4ade47aa14c2981f
RdotSilva <RyandotSilva@gmail.com>
2023-04-19 19:33:25 +0100

    Add transformMovie util

---
+++ src/utils/movies.ts
@@ -0,0 +1,21 @@
+import { IncomingMovie, Movie } from "../mocks/mockMovies";
+
+/**
+ * Transform a movie object from the TMDB API into an object useable in the UI
+ * @param movie Incoming movie data from TMDB API
+ * @returns A transformed movie object with the correct shape for the UI
+ */
+export const transformMovie = (movie: IncomingMovie): Movie => {
+  const imageUrl = "https://image.tmdb.org/t/p/w500";
+  const movieUrl = "https://www.themoviedb.org/movie";
+
+  const transformedMovie: Movie = {
+    image: `${imageUrl}${movie.backdrop_path}`,
+    title: movie.title,
+    description: movie.overview,
+    releaseDate: movie.release_date,
+    movieUrl: `${movieUrl}/${movie.id}`,
+  };
+
+  return transformedMovie;
+};
