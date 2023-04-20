0e880e6ed314ab6a7a42cebdc80083048c13cb52
RdotSilva <RyandotSilva@gmail.com>
2023-04-20 19:47:54 +0100

    Add new router implementation

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,14 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Home from "../Home/Home";
 import NewReleases from "../../pages/NewReleases/NewReleases";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/newreleases",
-    element: <NewReleases />,
-  },
-]);
+import Layout from "../Layout/Layout";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="newreleases" element={<NewReleases />} />
+    </Route>
+  )
+);
