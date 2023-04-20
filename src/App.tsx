bd8d55583c2a821c418299fb5292fa9b8ce1c37e
RdotSilva <RyandotSilva@gmail.com>
2023-04-20 19:48:03 +0100

    Remove Layout from main App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/Router/Router";
-import Layout from "./components/Layout/Layout";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Layout>
-      <RouterProvider router={router} />
-    </Layout>
+    <RouterProvider router={router} />
   </ChakraProvider>
 );
