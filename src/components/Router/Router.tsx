b69b635bd32d538a96f28919c2f2e624492b9890
RdotSilva <RyandotSilva@gmail.com>
2023-04-21 19:59:46 +0100

    Refactor router

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -7,12 +7,22 @@ import Home from "../Home/Home";
 import NewReleases from "../../pages/NewReleases/NewReleases";
 
 import Layout from "../Layout/Layout";
+import NotFound from "../../pages/NotFound/NotFound";
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="newreleases" element={<NewReleases />} />
-    </Route>
-  )
-);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "newreleases",
+        element: <NewReleases />,
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
