1c9b0659cbe27010ad3f7501dde8da0d48a39ca3
RdotSilva <RyandotSilva@gmail.com>
2023-04-21 20:01:58 +0100

    Move code back into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/Router/Router";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <ColorModeScript />
     <RouterProvider router={router} />
   </ChakraProvider>
 );
