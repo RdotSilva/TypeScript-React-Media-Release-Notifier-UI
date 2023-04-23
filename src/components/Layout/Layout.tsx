6d95eab82b28f77da064924baf34690796bb6bd1
RdotSilva <RyandotSilva@gmail.com>
2023-04-23 21:43:47 +0100

    Clean up unused imports

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import React from "react";
 import { NavBar } from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
