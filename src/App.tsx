49d5ed522e8fbc5d12947ba5fa6b1e2c11eba02e
RdotSilva <RyandotSilva@gmail.com>
2023-04-25 20:32:56 +0100

    Add queryClient to main App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import * as React from "react";
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/Router/Router";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <ColorModeScript />
-    <RouterProvider router={router} />
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript />
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </QueryClientProvider>
 );
