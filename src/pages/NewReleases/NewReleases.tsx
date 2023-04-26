40c66e2143d5760c41e0554bd1f30f402a4eeebc
RdotSilva <RyandotSilva@gmail.com>
2023-04-26 20:54:34 +0100

    Add template for axios request with react query

--- src/pages/NewReleases/NewReleases.tsx
+++ src/pages/NewReleases/NewReleases.tsx
@@ -1,19 +1,42 @@
 import { Box, Flex, StackDivider, Text, VStack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
-import { Movie, mockIncomingMovies, mockMovies } from "../../mocks/mockMovies";
+import {
+  IncomingMovie,
+  Movie,
+  mockIncomingMovies,
+  mockMovies,
+} from "../../mocks/mockMovies";
 import { transformMovie } from "../../utils/movies";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+// TODO: Update Axios call to use real API
+const fetchMovieData = async (): Promise<any> => {
+  const response = await axios.get("TODO");
+  return response.data;
+};
 
 const NewReleases = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
+  const { isLoading, error, data } = useQuery("movieData", fetchMovieData);
+
   useEffect(() => {
     setMovies(
-      mockIncomingMovies.map((incomingMovie) => {
+      data.map((incomingMovie: IncomingMovie) => {
         return transformMovie(incomingMovie);
       })
     );
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
 
   return (
     <>
