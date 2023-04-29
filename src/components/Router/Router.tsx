08a8b0a979f7064a686c6db6815aa3a66c09e290
RdotSilva <RyandotSilva@gmail.com>
2023-04-29 13:41:47 +0100

    Add new route for user options

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,13 +1,10 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../../pages/Home/Home";
 import NewReleases from "../../pages/NewReleases/NewReleases";
 
 import Layout from "../Layout/Layout";
 import NotFound from "../../pages/NotFound/NotFound";
+import UserOptions from "../../pages/UserOptions/UserOptions";
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: "newreleases",
         element: <NewReleases />,
+      },
+      {
+        path: "options",
+        element: <UserOptions />,
       },
       {
         path: "/",
